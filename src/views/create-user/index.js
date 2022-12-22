d7003c9fa7e228dfabae4c38643897f9eb0c6b07
Lory-Stan TANASI <stantanasi@gmail.com>
2022-12-22 16:09:37 +0100

    feat(create-user): add form to create an user

--- src/views/create-user/index.js
+++ src/views/create-user/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { Formik } from 'formik';
 import {
+  Box,
+  Button,
   Container,
+  TextField,
+  FormHelperText,
   makeStyles
 } from '@material-ui/core';
 import Page from 'src/components/Page';
@@ -20,6 +26,11 @@ const useStyles = makeStyles((theme) => ({
 
 function CreateUserView() {
   const classes = useStyles();
+  const history = useHistory();
+
+  const onUserCreated = () => {
+    history.push('/app/users');
+  };
 
   return (
     <Page
@@ -27,6 +38,97 @@ function CreateUserView() {
       title="Create user"
     >
       <Container maxWidth="md">
+        <Formik>
+          {({
+            errors,
+            handleBlur,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            touched,
+            values
+          }) => (
+            <form
+              noValidate
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                error={Boolean(touched.firstName && errors.firstName)}
+                fullWidth
+                autoFocus
+                helperText={touched.firstName && errors.firstName}
+                label="First name"
+                margin="normal"
+                name="firstName"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="name"
+                value={values.firstName}
+                variant="outlined"
+              />
+              <TextField
+                error={Boolean(touched.lastName && errors.lastName)}
+                fullWidth
+                autoFocus
+                helperText={touched.lastName && errors.lastName}
+                label="Last name"
+                margin="normal"
+                name="lastName"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="name"
+                value={values.lastName}
+                variant="outlined"
+              />
+              <TextField
+                error={Boolean(touched.email && errors.email)}
+                fullWidth
+                autoFocus
+                helperText={touched.email && errors.email}
+                label="Email Address"
+                margin="normal"
+                name="email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="email"
+                value={values.email}
+                variant="outlined"
+              />
+              <TextField
+                error={Boolean(touched.password && errors.password)}
+                fullWidth
+                helperText={touched.password && errors.password}
+                label="Password"
+                margin="normal"
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                type="password"
+                value={values.password}
+                variant="outlined"
+              />
+              <Box mt={2}>
+                <Button
+                  color="secondary"
+                  disabled={isSubmitting}
+                  fullWidth
+                  size="large"
+                  type="submit"
+                  variant="contained"
+                >
+                  Create
+                </Button>
+                {errors.submit && (
+                  <Box mt={3}>
+                    <FormHelperText error>
+                      {errors.submit}
+                    </FormHelperText>
+                  </Box>
+                )}
+              </Box>
+            </form>
+          )}
+        </Formik>
       </Container>
     </Page>
   );
