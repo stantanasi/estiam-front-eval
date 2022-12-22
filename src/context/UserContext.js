a0c100015a17171603481a950dff085504217d88
Bouteille158 <alexandre.benali@estiam.com>
2022-12-22 16:43:30 +0100

    fix: remove unused imports

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,20 +1,13 @@
-import React, {
-  createContext,
-  useState
-} from 'react';
+import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const UserContext = createContext();
 
 export function UserContextProvider({ children }) {
-  const [userName, setUserName] = useState('John Doe');
+  const [userName] = useState('John Doe');
 
   return (
-    <UserContext.Provider
-      value={userName}
-    >
-      {children}
-    </UserContext.Provider>
+    <UserContext.Provider value={userName}>{children}</UserContext.Provider>
   );
 }
 
--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,24 +1,13 @@
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useContext,
-  useMemo
-} from 'react';
-import {
-  Container,
-  makeStyles,
-  Box
-} from '@material-ui/core';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
+import { Container, makeStyles, Box } from '@material-ui/core';
 import Page from 'src/components/Page';
 import usersService from 'src/services/usersService';
 import useIsMountedRef from 'src/hooks/useIsMountedRef';
-import useAxios from 'src/hooks/useAxios';
 import UserContext from 'src/context/UserContext';
 import Header from './Header';
 import UsersTable from './UsersTable';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(6)
@@ -34,29 +23,25 @@ function UsersListView() {
   const [users, setUsers] = useState([]);
 
   const getUsers = useCallback(() => {
-    usersService.listUsers()
-      .then((res) => {
-        if (isMountedRef.current) {
-          setUsers(res);
-          setUsersCount(res.length);
-        }
-      });
-  }, []);
+    usersService.listUsers().then(res => {
+      if (isMountedRef.current) {
+        setUsers(res);
+        setUsersCount(res.length);
+      }
+    });
+  }, [isMountedRef]);
 
   useEffect(() => {
     console.log('Test react context: ');
     console.log(userName);
 
     getUsers();
-  }, [getUsers]);
+  }, [getUsers, userName]);
 
   const testButtonClicked = () => {};
 
   return (
-    <Page
-      className={classes.root}
-      title="Users List"
-    >
+    <Page className={classes.root} title="Users List">
       <Container>
         <Header usersCount={usersCount} />
 
