abbdbc20fe839ffa4f1a67daa0d3538a6ad8b3d9
Lory-Stan TANASI <stantanasi@gmail.com>
2022-12-22 12:46:09 +0100

    feat(users-service): send access token on request

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { API_BASE_URL_SEC } from 'src/config';
+import authService from './authService';
 
 class UsersService {
   listUsers = () => new Promise((resolve, reject) => {
     axios({
       method: 'GET',
       url: `${API_BASE_URL_SEC}/v1/users`,
-      // headers: {}
+      headers: {
+        'x-access-token': authService.getAccessToken(),
+      },
     })
       .then((res) => {
         resolve(res.data);
