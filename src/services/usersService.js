5a76afc81d2fda0c2085c0b19a3fb7228013d24f
Lory-Stan TANASI <stantanasi@gmail.com>
2022-12-22 12:45:09 +0100

    feat(users-service): use correct api base url

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import { API_BASE_URL } from 'src/config';
+import { API_BASE_URL_SEC } from 'src/config';
 
 class UsersService {
   listUsers = () => new Promise((resolve, reject) => {
     axios({
       method: 'GET',
-      url: `${API_BASE_URL}/users`,
+      url: `${API_BASE_URL_SEC}/v1/users`,
       // headers: {}
     })
       .then((res) => {
